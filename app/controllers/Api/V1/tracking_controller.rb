91654dfc41ae4fd013c3aee552649d1f923965d4
Daniel Andrade <dam@unixmexico.org>
2020-08-20 09:30:34 -0500

    Probamos que nuestro controlador funcione

--- app/controllers/Api/V1/tracking_controller.rb
+++ app/controllers/Api/V1/tracking_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::TrackingController < ApplicationController
 
   def track
+    requests = JSON.parse(params["guias"])
+
+    results = "Procesando..."
+    render json: results
 
   end
 end
